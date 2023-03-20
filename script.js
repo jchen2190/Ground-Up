6a26008f05a02b3c163cdbd355ed58ad66dd30f3
Jon Chen <jchen2190@gmail.com>
2023-03-20 18:00:29 -0400

    trim lines

--- script.js
+++ script.js
@@ -5,25 +5,18 @@ let featureText = document.querySelectorAll('.features .text');
 ScrollTrigger.matchMedia( {
     '(min-width:750px)': function() {
         for (let i=0; i < featureImg.length; i++) {
-    
-    // if [i] is an even number (0,2)
-        let movement = 100;
-        if (i % 2 == 0) {
-            movement = -100;  // even rows
-        }	
-
-        // animate current image each time thru the loop
-        gsap.from(featureImg[i], {scrollTrigger: 
-            {trigger: featureImg[i], start: 'top 85%', end: 'bottom 80%', scrub: 1, },
-            x: -movement,  // odd rows
-            // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+            let movement = 100;
+            if (i % 2 == 0) {
+                movement = -100;
+            }	
+            gsap.from(featureImg[i], {scrollTrigger: 
+                {trigger: featureImg[i], start: 'top 85%', end: 'bottom 80%', scrub: 1, },
+                x: -movement,
+                opacity: 0 } );
+            gsap.from(featureText[i], {scrollTrigger: 
+            {trigger: featureText[i], start: 'top 85%', end: 'bottom 80%', scrub: 1,  },
+            x: movement,
             opacity: 0 } );
-        
-        // animate current text each time thru the loop
-        gsap.from(featureText[i], {scrollTrigger: 
-        {trigger: featureText[i], start: 'top 85%', end: 'bottom 80%', scrub: 1,  }, // markers: true
-        x: movement,  // odd rows
-        opacity: 0 } );
         }; 
     }
 } );
